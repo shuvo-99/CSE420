4b6283b16dd0e996ec4fb4e603d856122c93ab45
shuvo-99 <anontoshuvo99@gmail.com>
2023-04-17 01:21:15 +0600

    node no. done

--- 420 project.py
+++ 420 project.py
@@ -1,3 +1,21 @@
+class Node:
+  node = 0
+  def __init__(self, symbol):
+    self.symbol = symbol
+    self.left = None
+    self.right = None
+    self.nullable = False
+    self.firstpos = set()
+    self.lastpos = set()
+    self.node_No = self.node_no(symbol)
+  
+  def node_no(self,symbol):
+    if symbol.isalpha() or symbol == '#':
+      Node.node += 1
+      return Node.node
+    
+    # print(node_no)
+
 def augmentation(regex):
   if regex[-1] != '#':
     regex = regex + '#'
@@ -6,3 +24,8 @@ def augmentation(regex):
 regex = '(a|b)*abb#'
 augmented_regex = augmentation(regex)
 print(augmented_regex)
+
+for i in regex:
+  obj = Node(i)
+  print(obj.node_No)
+
