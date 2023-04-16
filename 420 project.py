c1e2a9855961f750721571c3bfa63b23c9049957
shuvo-99 <anontoshuvo99@gmail.com>
2023-04-17 02:14:41 +0600

    node no.

--- 420 project.py
+++ 420 project.py
@@ -1,31 +1,54 @@
-class Node:
-  node = 0
-  def __init__(self, symbol):
-    self.symbol = symbol
-    self.left = None
-    self.right = None
-    self.nullable = False
-    self.firstpos = set()
-    self.lastpos = set()
-    self.node_No = self.node_no(symbol)
+# class Node:
   
-  def node_no(self,symbol):
-    if symbol.isalpha() or symbol == '#':
-      Node.node += 1
-      return Node.node
+#   # nullable = False
+
+#   def __init__(self, symbol):
+#     self.regex = symbol
+#     self.left = None
+#     self.right = None
+#     self.nullable = self.isNullable(symbol)
+#     print(self.nullable)
+#     self.firstpos = set()
+#     self.lastpos = set()
+#     self.node_No = self.node_no(symbol)
+#     print(self.nullable)
+
+#   def isNullable(self,symbol):
+#     if symbol.isalpha() or symbol == '#':
+#       # self.nullable = False
+#       return False
+#     else:
+#       # self.nullable = True
+#       return True
     
-    # print(node_no)
+      # return .nullable
+
+
+# for i in regex:
+#   obj = Node(i)
+#   print(obj.node_No)
+
+# obj = Node(regex)
+
+
 
 def augmentation(regex):
   if regex[-1] != '#':
     regex = regex + '#'
   return regex
 
+def node_no(symbol):
+  global node
+  if symbol.isalpha() or symbol == '#':
+    node += 1
+    node_dict[node] = symbol
+
 regex = '(a|b)*abb#'
 augmented_regex = augmentation(regex)
 print(augmented_regex)
 
+node = 0
+node_dict = {}
 for i in regex:
-  obj = Node(i)
-  print(obj.node_No)
-
+  node_no(i)
+print(node_dict)
